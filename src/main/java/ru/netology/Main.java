00d61f9d831395b72af6fdfb874ddc7d13bcc447
Artem Chernyshov <silence_made@mail.ru>
2023-09-19 23:43:40 +0300

    Task2 is ready

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -6,7 +6,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         var server = new Server(9999, 64);
+
+
+        // добавление хендлеров (обработчиков)
+        server.addHandler("GET", "/messages",
+                (request, responseStream) -> {
+                    server.resourceNotFound(responseStream);
+                });
+
+
+        server.addHandler("POST", "/messages",
+                (request, responseStream) -> {
+                    server.resourceNotFound(responseStream);
+                });
+
         server.start();
+
 
     }
 }
